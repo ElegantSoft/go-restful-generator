5df1fd6e108d3d9419e6cc2115f73068e1df7999
ElegantSoft <react.laravel.dev@gmail.com>
2022-09-16 07:24:09 +0200

    try caller2

--- generators/init_project.go
+++ generators/init_project.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
 )
 
 var wd, _ = os.Getwd()
@@ -29,6 +30,9 @@ func Execute() {
 	}
 	data := Data{PackageName: result}
 	log.Printf("Prompt %v", filepath.Join("$GOPATH", "src", "github.com"))
+	_, filename, _, _ := runtime.Caller(-1)
+	dirname := filepath.Dir(filename)
+	log.Printf("Dir name is %s\n", dirname)
 
 	writetemplate.ProcessTemplate("templates/main.tmpl", "main.tmpl", filepath.Join("_example", "main.go"), data)
 }
--- pkg/writetemplate/process_template.go
+++ pkg/writetemplate/process_template.go
@@ -8,8 +8,6 @@ import (
 	"go/format"
 	"log"
 	"os"
-	"path/filepath"
-	"runtime"
 	"text/template"
 )
 
@@ -27,9 +25,6 @@ func ProcessTemplate(templatePath string, fileName string, outputPath string, da
 	fmt.Println("Writing file: ", outputPath)
 	f, _ := os.Create(outputPath)
 	w := bufio.NewWriter(f)
-	_, filename, _, _ := runtime.Caller(0)
-	dirname := filepath.Dir(filename)
-	log.Printf("Dir name is %s\n", dirname)
 	w.WriteString(string(formatted))
 	w.Flush()
 }
