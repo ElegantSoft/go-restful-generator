864aa2cc6e6fc25fe376b15835d9fc69424345b6
ElegantSoft <react.laravel.dev@gmail.com>
2022-10-21 06:37:08 +0200

    add  operator fix

--- crud/utils.go
+++ crud/utils.go
@@ -41,7 +41,7 @@ func (q *QueryToDBConverter) searchMapper(s map[string]interface{}, tx *gorm.DB)
 									if operatorKey == NotNullOperator || operatorKey == IsNullOperator {
 										tx.Where(fmt.Sprintf("%s %s", whereField, operator))
 									} else if operatorKey == InOperator {
-										tx.Where(fmt.Sprintf("%s in", whereField), []interface{}{value})
+										tx.Where(fmt.Sprintf("%s IN ?", whereField), []string{value.(string)})
 									} else {
 
 										if operatorKey == ContainOperator {
@@ -81,9 +81,9 @@ func (q *QueryToDBConverter) searchMapper(s map[string]interface{}, tx *gorm.DB)
 										}
 									} else if operatorKey == InOperator {
 										if i == 0 {
-											tx.Where(fmt.Sprintf("%s in", whereField), []interface{}{value})
+											tx.Where(fmt.Sprintf("%s IN ?", whereField), []string{value.(string)})
 										} else {
-											tx.Where(fmt.Sprintf("%s in", whereField), []interface{}{value})
+											tx.Where(fmt.Sprintf("%s IN ?", whereField), []string{value.(string)})
 										}
 									} else {
 										if operatorKey == ContainOperator {
